5987066b117abe75ba277cafcc1e10532a1ae8ab
m-aliabbas <vynz420@gmail.com>
2023-05-11 01:48:18 -0400

    Latest Model with Burger and Sandwitches

--- actions/actions.py
+++ actions/actions.py
@@ -2,15 +2,15 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-class ActionConfirmPizzaOrder(Action):
-    def name(self) -> Text:
-        return "utter_confirm_pizza_order"
+# class ActionConfirmPizzaOrder(Action):
+#     def name(self) -> Text:
+#         return "utter_confirm_pizza_order"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        pizza_type = tracker.get_slot("pizza_type")
-        pizza_size = tracker.get_slot("pizza_size")
-        dispatcher.utter_message(text=f"Got it! You've ordered a {pizza_size} {pizza_type} pizza. Would you like to add anything else?")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         pizza_type = tracker.get_slot("pizza_type")
+#         pizza_size = tracker.get_slot("pizza_size")
+#         dispatcher.utter_message(text=f"Got it! You've ordered a {pizza_size} {pizza_type} pizza. Would you like to add anything else?")
+#         return []
 
 class ActionConfirmDrinkOrder(Action):
     def name(self) -> Text:
