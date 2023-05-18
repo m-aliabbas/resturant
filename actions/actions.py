e8693c0433392587fbfd06294547406d9bdafc1e
m-aliabbas <vynz420@gmail.com>
2023-05-18 03:10:30 -0400

    model training

--- actions/actions.py
+++ actions/actions.py
@@ -45,6 +45,7 @@ class ActionMakeOrder(Action):
                     food_order += [entity['value']]
                     food_order.append(entity['value'])
             food_order += existing_food_order
+
             dispatcher.utter_message(text=f"You have selected {food_order} from food order")
             return [SlotSet("food_order", food_order)]
         return []
@@ -64,7 +65,7 @@ class ActionMakeOrder(Action):
         intent = latest_message.get("intent", {}).get("name")
         print(f"----------------- {intent}-------------------------------")
         print(existing_food_order)
-        food_order= existing_food_order
+        food_order = existing_food_order
         if intent == 'order_food':
             print(entities)
             if len(entities)>0:
@@ -72,9 +73,11 @@ class ActionMakeOrder(Action):
                     food_order += [entity['value']]
                     food_order.append(entity['value'])
             food_order += existing_food_order
-            dispatcher.utter_message(text=f"You have selected {food_order} from food order")
-            return [SlotSet("food_order", food_order)]
-        return []
+            
+            dispatcher.utter_message(text=f"You have selected {food_order} from food order.")
+            
+        food_order = list(set(food_order))
+        return [SlotSet("food_order", food_order)]
 
 class ActionAddAddress(Action):
     def name(self) -> Text:
@@ -112,38 +115,5 @@ class ActionAddDrink(Action):
         
         dispatcher.utter_message(text=f"You have added drink")
         return []
-# class ActionConfirmPizzaOrder(Action):
-#     def name(self) -> Text:
-#         return "utter_confirm_pizza_order"
 
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         pizza_type = tracker.get_slot("pizza_type")
-#         pizza_size = tracker.get_slot("pizza_size")
-#         dispatcher.utter_message(text=f"Got it! You've ordered a {pizza_size} {pizza_type} pizza. Would you like to add anything else?")
-#         return []
 
-# class ActionConfirmDrinkOrder(Action):
-#     def name(self) -> Text:
-#         return "utter_confirm_drink_order"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         drink = tracker.get_slot("drink")
-#         dispatcher.utter_message(text=f"Alright! We've added a {drink} to your order. Anything else?")
-#         return []
-
-# class ActionConfirmAddress(Action):
-#     def name(self) -> Text:
-#         return "utter_confirm_address"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         address = tracker.get_slot("address")
-#         dispatcher.utter_message(text=f"Thank you for providing your address: {address}. Your order will be delivered there.")
-#         return []
-
-# class ActionProvideDeliveryTime(Action):
-#     def name(self) -> Text:
-#         return "utter_provide_delivery_time"
-
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         dispatcher.utter_message(text="The estimated delivery time is 30-45 minutes. Enjoy your meal!")
-#         return []
